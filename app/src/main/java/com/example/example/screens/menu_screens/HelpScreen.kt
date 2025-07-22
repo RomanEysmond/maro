2229fc77a69f5343e0801f2ce047b38fe6025f4f
RomanEysmond <eysmondroman@gmail.com>
2025-07-22 10:33:05 +0300

    HelpScreen changed

--- app/src/main/java/com/example/example/screens/menu_screens/HelpScreen.kt
+++ app/src/main/java/com/example/example/screens/menu_screens/HelpScreen.kt
@@ -1,8 +1,13 @@
 package com.example.example.screens.menu_screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 
 @Composable
 fun HelpScreen() {
-
+    Column {
+        Text("wdw")
+    }
 }
