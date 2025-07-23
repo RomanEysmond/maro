f5cc05560b500488812bf7597c4670cf1ff03560
RomanEysmond <eysmondroman@gmail.com>
2025-07-23 19:41:29 +0300

    Profile screen created

--- app/src/main/java/com/example/example/screens/ChatsPagingScreen.kt
+++ app/src/main/java/com/example/example/screens/ChatsPagingScreen.kt
@@ -11,9 +11,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.example.R
+import com.example.example.screens.menu_screens.HelpScreen
+import com.example.example.screens.menu_screens.ProfileScreen
+import com.example.example.screens.menu_screens.SettingsScreen
 import com.skydoves.landscapist.glide.GlideImage
 import kotlinx.coroutines.launch
+
+enum class MenuScreens {
+    ProfileScreen,
+    SettingsScreen,
+    HelpScreen,
+    MoreCharacter,
+    Location
+}
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -21,11 +36,27 @@ fun ChatsPagingScreen() {
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
     var selectedItem by remember { mutableStateOf(0) }
+    val menuItems = mutableListOf<Triple<Int, String, String>>(
+        Triple(
+            android.R.drawable.ic_menu_myplaces, stringResource(R.string.img1_text),
+            MenuScreens.ProfileScreen.name
+        ),
+        Triple(
+            android.R.drawable.ic_menu_manage, stringResource(R.string.img2_text),
+            MenuScreens.SettingsScreen.name
+        ),
+        Triple(
+            android.R.drawable.ic_menu_info_details, stringResource(R.string.img3_text),
+            MenuScreens.HelpScreen.name
+        )
+    )
+
 
     val menuItems2 = mutableListOf<Pair<Int, String>>(
         Pair(android.R.drawable.ic_menu_myplaces, stringResource(R.string.img1_text)),
         Pair(android.R.drawable.ic_menu_manage, stringResource(R.string.img2_text)),
-        Pair(android.R.drawable.ic_menu_info_details, stringResource(R.string.img3_text)))
+        Pair(android.R.drawable.ic_menu_info_details, stringResource(R.string.img3_text))
+    )
 
     Scaffold(
         scaffoldState = scaffoldState,
@@ -67,7 +98,8 @@ fun ChatsPagingScreen() {
 fun DrawerContent(
     items: List<Pair<Int, String>>,
     selectedItem: Int,
-    onItemClick: (Int) -> Unit
+    onItemClick: (Int) -> Unit,
+    navController: NavHostController = rememberNavController()
 ) {
     Column(modifier = Modifier.fillMaxSize()) {
         // Заголовок меню
@@ -84,8 +116,21 @@ fun DrawerContent(
             ListItem(
                 text = { Text(item.second) },
                 icon = { GlideImage(item.first, modifier = Modifier.size(35.dp)) },
-
             )
+            NavHost(
+                navController = navController,
+                startDestination = MenuScreens.SettingsScreen.name
+            ) {
+                composable(route = MenuScreens.ProfileScreen.name) {
+                    ProfileScreen()
+                }
+                composable(route = MenuScreens.SettingsScreen.name) {
+                    SettingsScreen()
+                }
+                composable(route = MenuScreens.HelpScreen.name) {
+                    HelpScreen()
+                }
+            }
         }
     }
 }
